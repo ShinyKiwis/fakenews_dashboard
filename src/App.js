8823cc538ec44fc7ca5beee5a25a95adc6dda5cd
ShinyKiwis <nguyenhua.hcm@gmail.com>
2022-10-11 22:11:08 +0700

    Updated: Add MainContent component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import MainContent from './components/MainContent/MainContent';
 import SideBar from './components/SideBar/SideBar';
 
 function App() {
   return (
     <div>
       <SideBar /> 
+      <MainContent />
     </div>
   );
 }
---
+++ src/components/MainContent/MainContent.js
@@ -0,0 +1,11 @@
+import "./MainContent.css"
+
+const MainContent = () => {
+    return (
+        <div className="container">
+            Main Content
+        </div>
+    )
+}
+
+export default MainContent
